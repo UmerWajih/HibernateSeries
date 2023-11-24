f5d51e637ee77476865a470490e42b38930da761
Umer <umer.wajih@globalnorthstar.com>
2023-11-24 23:34:47 +0500

    init

---
+++ src/main/java/hibernate/test/hibernatetest/App.java
@@ -0,0 +1,13 @@
+package hibernate.test.hibernatetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
