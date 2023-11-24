eb76ade4388f5916d592f0a780ed9e66e248f4f7
Umer <umer.wajih@globalnorthstar.com>
2023-11-25 01:33:41 +0500

    Java properties

--- src/main/java/hibernate/test/hibernatetest/App.java
+++ src/main/java/hibernate/test/hibernatetest/App.java
@@ -1,5 +1,7 @@
 package hibernate.test.hibernatetest;
 
+import java.util.Properties;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,11 +14,21 @@ import org.hibernate.cfg.Configuration;
 public class App 
 {
     public static void main( String[] args )
-    {
-        Configuration conf = new Configuration().configure().addAnnotatedClass(UserTable.class);
+    { 	
+    	Properties prop = new Properties();
+    	prop.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+    	prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/hibernatetestDB");
+    	prop.setProperty("hibernate.connection.username", "root");
+    	prop.setProperty("hibernate.connection.password", "");
+    	prop.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+    	prop.setProperty("hibernate.hbm2ddl.auto", "create");
+    	
+    	Configuration conf = new Configuration().addProperties(prop).addAnnotatedClass(UserTable.class);
         SessionFactory factory = conf.buildSessionFactory();
         Session session = factory.openSession();
         session.beginTransaction();
+        session.save(new UserTable());
+        session.save(new UserTable());
         session.save(new UserTable());
         session.getTransaction().commit();
         session.close();
